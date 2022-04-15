51fc7da597bb0cc6b3e80076c190f2379fdadf9a
hannahchun <hannahchun2000@gmail.com>
2022-04-15 23:35:18 +0900

    add manager-search product by name

--- manager.c
+++ manager.c
@@ -17,3 +17,22 @@ int selectDataNo(Product *p, int count){
     return no;
 }
 
+void searchName(Product *p, int count){
+    int scnt=0;
+    char searchname[20];
+    ClearReadBuffer();
+    printf("\n검색할 과일 이름? ");
+    fgets(searchname,sizeof(searchname),stdin);
+    searchname[strlen(searchname)-1]='\0';
+
+    for(int i=0 ; i<count ; i++){
+        if(p[i].price==-1) continue;
+        if(strstr(p[i].name, searchname)){
+            printf("\n");
+            readProduct(p[i]);
+            scnt++;
+        }
+    }
+    if(scnt==0) printf("\n=>검색된 데이터 없음!\n");
+}
+
